0a629e1e5e0b80d52360cf62ecab3b450fddf3f7
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-14 12:59:13 +0300

    Created Model Product

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    TOYS = 'Toys'
+    FURNITURE = 'Furniture'
+    ELECTRONICS = 'Electronics'
+    HOUSEHOLD = 'Household'
+    VEHICLE = 'Vehicle'
+    PRODUCT_CATEGORIES = [
+        (TOYS, 'Toys'),
+        (FURNITURE, 'Furniture'),
+        (ELECTRONICS, 'Electronics'),
+        (HOUSEHOLD, 'Household'),
+        (VEHICLE, 'Vehicle')
+    ]
+    vendor = models.ForeignKey(
+        'auth.user', related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    category = models.CharField(choices=PRODUCT_CATEGORIES, max_length=50)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.title
