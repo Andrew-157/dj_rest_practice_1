6d4025725a43aea5547346ed7335c7d1112ed37c
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-14 13:05:51 +0300

    Changed model Product

--- app1/models.py
+++ app1/models.py
@@ -20,6 +20,10 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     category = models.CharField(choices=PRODUCT_CATEGORIES, max_length=50)
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['-posted']
