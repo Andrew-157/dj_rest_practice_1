a47ae98432ce0a33b4b1d84a6c5ad756f8d66d23
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-19 10:53:23 +0300

    Created Model Story

--- stories/models.py
+++ stories/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Story(models.Model):
+    title = models.CharField(max_length=55)
+    content = models.TextField()
+    writer = models.ForeignKey(
+        'auth.User', related_name='stories', on_delete=models.CASCADE)
+    pub_date = models.DateField(auto_now=True)
