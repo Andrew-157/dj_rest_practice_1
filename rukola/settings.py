966099f549931798ee89ad9ae7e09bc2cb32de5d
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-19 10:07:30 +0300

    Connected to mysql database

--- rukola/settings.py
+++ rukola/settings.py
@@ -73,10 +73,14 @@ WSGI_APPLICATION = 'rukola.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rukola',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'mysqlpassword'
     }
 }
 
